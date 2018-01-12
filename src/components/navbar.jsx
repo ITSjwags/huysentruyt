2dd22b0120a1f805ab236ed115d25a3e7bcaacf6
ITSjwags <jvwagoner@gmail.com>
2018-01-11 21:23:19 -0800

    Finish up mobile menu and smooth scrolling to anchor

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
+import { Link as ScrollLink } from 'react-scroll';
 // styles
 import '../styles/navbar.scss';
+// images
+import iconDribbble from '../images/icon-dribbble.png';
+import iconFacebook from '../images/icon-facebook.png';
+import iconInstagram from '../images/icon-instagram.png';
+import iconLinkedin from '../images/icon-linkedin.png';
+import iconTwitter from '../images/icon-twitter.png';
+import iconYoutube from '../images/icon-youtube.png';
 
 class Navbar extends Component {
 
@@ -9,33 +17,64 @@ class Navbar extends Component {
     super(props);
 
     this.state = {
-      isActive: false
+      isActive: false,
+      menuHeight: 0
     }
   }
 
+  componentDidMount() {
+    this.getMenuHeight();
+    window.addEventListener('resize', this.getMenuHeight);
+    window.addEventListener('scroll', this.getMenuHeight);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.getMenuHeight);
+    window.removeEventListener('scroll', this.getMenuHeight);
+  }
+
   onToggleNavButton() {
-    this.setState({ isActive: !this.state.isActive });
+    this.getMenuHeight();
+
+    this.setState({
+      isActive: !this.state.isActive
+    });
+
+    if (!this.state.isActive) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  getMenuHeight = () => {
+    const bodyHeight = window.innerHeight;
+    const menuHeight = bodyHeight - this.refs.navbar.offsetHeight;
+
+    this.setState({ menuHeight: menuHeight });
+  }
+
+  handleScrollLinkClick = () => {
+    this.onToggleNavButton();
   }
 
   render() {
-    const { isActive } = this.state;
+    const { isActive, menuHeight } = this.state;
 
     return (
-      <div className="Navbar">
+      <div className={`Navbar ${isActive ? 'is-active' : ''}`} ref="navbar">
         <div>
           <Link
             className="Navbar-logo"
             to="/"
           >
             J<span className="fullname">effrey</span>.
-        </Link>
+          </Link>
         </div>
 
         <nav role="navigation">
           <div className={`Navbar-button ${isActive ? 'is-active' : ''}`}>
             <button
-              aria-expanded="false"
-              aria-controls="menu"
               className={`hamburger ${isActive ? 'is-active' : ''}`}
               onClick={() => this.onToggleNavButton()}
             >
@@ -45,18 +84,125 @@ class Navbar extends Component {
             </button>
           </div>
 
-          <div className={`Navbar-list ${isActive ? 'is-active' : ''}`}>
+          <div className="Navbar-list">
             <ul>
               <li>
-                <a href="#about" className="Navbar-listLink">About</a>
+                <ScrollLink
+                  className="Navbar-listLink"
+                  to="about"
+                  smooth={"easeInOutQuad"}
+                  duration={750}
+                >
+                  About
+                </ScrollLink>
               </li>
               <li>
-                <a href="#work" className="Navbar-listLink">Work</a>
+                <ScrollLink
+                  className="Navbar-listLink"
+                  to="work"
+                  smooth={"easeInOutQuad"}
+                  duration={750}
+                >
+                  Work
+                </ScrollLink>
               </li>
               <li>
-                <a href="#contact" className="Navbar-listLink">Contact</a>
+                <ScrollLink
+                  className="Navbar-listLink"
+                  to="contact"
+                  smooth={"easeInOutQuad"}
+                  duration={750}
+                >
+                  Contact
+                </ScrollLink>
               </li>
             </ul>
+          </div>
+
+          <div className={`Navbar-list-mobile g-bg-change ${isActive ? 'is-active' : ''}`}>
+            <ul>
+              <li>
+                <h2>
+                  <ScrollLink
+                    className="Navbar-listLink"
+                    to="about"
+                    smooth={"easeInOutQuad"}
+                    duration={750}
+                    onClick={this.handleScrollLinkClick}
+                    delay={250}
+                  >
+                    About
+                  </ScrollLink>
+                </h2>
+              </li>
+              <li>
+                <h2>
+                  <ScrollLink
+                    className="Navbar-listLink"
+                    to="work"
+                    smooth={"easeInOutQuad"}
+                    duration={750}
+                    onClick={this.handleScrollLinkClick}
+                    delay={250}
+                  >
+                    Work
+                  </ScrollLink>
+                </h2>
+              </li>
+              <li>
+                <h2>
+                  <ScrollLink
+                    className="Navbar-listLink"
+                    to="contact"
+                    smooth={"easeInOutQuad"}
+                    duration={750}
+                    onClick={this.handleScrollLinkClick}
+                    delay={250}
+                  >
+                    Contact
+                  </ScrollLink>
+                </h2>
+              </li>
+            </ul>
+
+            <div className="Navbar-list-mobile-footer">
+              <ul className="Navbar-list-mobile-socials">
+                <li>
+                  <a href="https://www.linkedin.com/in/therealmcjeffy/" target="_blank">
+                    <img src={iconLinkedin} />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.facebook.com/jhuysentruyt" target="_blank">
+                    <img src={iconFacebook} />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/therealmcjeffy" target="_blank">
+                    <img src={iconTwitter} />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/therealmcjeffy/" target="_blank">
+                    <img src={iconInstagram} />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://dribbble.com/therealmcjeffy" target="_blank">
+                    <img src={iconDribbble} />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/channel/UC82RsyFkZrFGOnvMznkHGsA" target="_blank">
+                    <img src={iconYoutube} />
+                  </a>
+                </li>
+              </ul>
+
+              <a className="Navbar-list-resume" href="#">
+                Full Resume
+              </a>
+            </div>
           </div>
         </nav>
 
