0f0a485f5dec48492c26d0a00aad78f53a8b790c
ITSjwags <jvwagoner@gmail.com>
2018-01-28 18:54:20 -0800

    Turn footer component into a class to handle local state so we can style hovering stuff

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,49 +10,105 @@ import iconLinkedin from '../images/icon-linkedin.png';
 import iconTwitter from '../images/icon-twitter.png';
 import iconYoutube from '../images/icon-youtube.png';
 
-const Footer = () => (
-  <div className="Footer g-bg-change">
-    <div className="Footer-content">
-      <ul className="Footer-list">
-        <li>
-          <a href="https://www.linkedin.com/in/therealmcjeffy/" target="_blank">
-            <img src={iconLinkedin} />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/jhuysentruyt" target="_blank">
-            <img src={iconFacebook} />
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/therealmcjeffy" target="_blank">
-            <img src={iconTwitter} />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com/therealmcjeffy/" target="_blank">
-            <img src={iconInstagram} />
-          </a>
-        </li>
-        <li>
-          <a href="https://dribbble.com/therealmcjeffy" target="_blank">
-            <img src={iconDribbble} />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/channel/UC82RsyFkZrFGOnvMznkHGsA" target="_blank">
-            <img src={iconYoutube} />
-          </a>
-        </li>
-      </ul>
+class Footer extends Component {
+  constructor(props) {
+    super(props);
 
-      <div className="Footer-right">
-        <a href="#" className="Footer-download">
-          Resume
-        </a>
+    this.state = {
+      isHovering: false
+    }
+  }
+
+  onMouseEnter = () => {
+    this.setState({ isHovering: true })
+  }
+
+  onMouseLeave = () => {
+    this.setState({ isHovering: false })
+  }
+
+  render() {
+    const { isHovering } = this.state;
+
+    return (
+      <div className="Footer g-bg-change">
+        <div className="Footer-content">
+          <ul className={`Footer-list${isHovering ? ' is-hovering' : ''}`}>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/therealmcjeffy/"
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                target="_blank"
+              >
+                <img src={iconLinkedin} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/jhuysentruyt"
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                target="_blank"
+              >
+                <img src={iconFacebook} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/therealmcjeffy"
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                target="_blank"
+              >
+                <img src={iconTwitter} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/therealmcjeffy/"
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                target="_blank"
+              >
+                <img src={iconInstagram} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://dribbble.com/therealmcjeffy"
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                target="_blank"
+              >
+                <img src={iconDribbble} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UC82RsyFkZrFGOnvMznkHGsA"
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                target="_blank"
+              >
+                <img src={iconYoutube} />
+              </a>
+            </li>
+          </ul>
+
+          <div className="Footer-right">
+            <a href="#" className="Footer-download">
+              Resume
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
+
+Footer.propTypes = {
+
+};
 
 export default Footer;
